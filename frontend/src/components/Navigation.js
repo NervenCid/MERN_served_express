cf532ca207be8f4a2ef9b0fbf2a891ae43d7f969
NervenCid <nervencid@gmail.com>
2020-10-04 20:27:10 -0500

    Prueba para jenkins 5

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -11,7 +11,7 @@ export default class Navigation extends Component {
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container">
                     <Link className="navbar-brand" to="/">
-                        Aplicación de Notas (Modificado)
+                        Aplicación de Notas
                 </Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
@@ -19,13 +19,13 @@ export default class Navigation extends Component {
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item active">
-                                <Link className="nav-link" to="/">Notas (Modificado)</Link>                                
+                                <Link className="nav-link" to="/">Notas</Link>                                
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/create">Crear Nota (Modificado)</Link>
+                                <Link className="nav-link" to="/create">Crear Nota</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/user">Crear Usuario (Modificado)</Link>
+                                <Link className="nav-link" to="/user">Crear Usuario</Link>
                             </li>
                         </ul>
                     </div>
